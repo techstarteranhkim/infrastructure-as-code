981add8f99c0f6feb718654ebb5c6eef4924517a
techstarteranhkim <anh.kim@docc.techstarter.de>
2024-07-24 15:29:58 +0200

    working homework

--- terraform/1_einfache_ec2/main.tf
+++ terraform/1_einfache_ec2/main.tf
@@ -10,3 +10,16 @@ resource "aws_instance" "erste_instanz" {
   instance_type = "t2.nano"
   ami = "ami-01e444924a2233b07"
 }
+
+resource "aws_security_group" "ssh" {
+  name        = "ssh"
+  description = "Allow SSH traffic"
+  
+  # Inbound rule for SSH (port 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust this to restrict the source IP range if needed
+  }
+}
